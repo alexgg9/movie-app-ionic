23fed42b67005e0da0808669b81c6323828a65c3
alexgg9 <alexgalvezgarciaqt@gmail.com>
2024-06-13 12:26:02 +0200

    ionSpinners

--- src/app/pages/logincallback/logincallback.page.ts
+++ src/app/pages/logincallback/logincallback.page.ts
@@ -1,16 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth.service';
+import { IonicModule } from '@ionic/angular';
 
 @Component({
   selector: 'app-logincallback',
   templateUrl: './logincallback.page.html',
   styleUrls: ['./logincallback.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonicModule, CommonModule, FormsModule]
 })
 export class LogincallbackPage implements OnInit {
 
--- src/app/pages/movie-profile/movie-profile.page.ts
+++ src/app/pages/movie-profile/movie-profile.page.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { TmdbapiService } from '../../services/tmdbapi.service';
 import { FavoriteService } from '../../services/favorite.service';
-import { IonicModule } from '@ionic/angular';
+import { AlertController,IonicModule } from '@ionic/angular';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Movie } from 'src/app/models/movie';
@@ -24,11 +24,13 @@ export class MovieProfilePage implements OnInit {
   isFavorite: boolean = false;
   accountId: string | null = '';
   sessionId: string | null = '';
+  isLoading: boolean = true;
 
   constructor(
     private route: ActivatedRoute,
     private tmdbapiService: TmdbapiService,
-    private favoriteService: FavoriteService
+    private favoriteService: FavoriteService,
+    private alertController: AlertController
   ) {
     addIcons({ star, starOutline });
   }
@@ -43,6 +45,10 @@ export class MovieProfilePage implements OnInit {
         this.cast = data.credits.cast;
         this.backgroundImage = `url(https://image.tmdb.org/t/p/w780/${data.backdrop_path})`;
         this.members = data.credits.cast.slice(0, 5);
+        
+        setTimeout(() => {
+          this.isLoading = false;
+        }, 1000);
 
         if (this.accountId && this.sessionId) {
           this.favoriteService.getFavoriteMovies(this.accountId, this.sessionId).subscribe(favorites => {
@@ -53,12 +59,32 @@ export class MovieProfilePage implements OnInit {
     }
   }
 
-  toggleFavorite() {
+  async toggleFavorite() {
     if (this.movie && this.accountId && this.sessionId) {
       if (this.isFavorite) {
-        this.favoriteService.removeFavoriteMovie(this.accountId, this.sessionId, this.movie.id).subscribe(() => {
-          this.isFavorite = false;
+        const alert = await this.alertController.create({
+          header: 'Confirm',
+          message: 'Are you sure you want to remove this movie from favorites?',
+          cssClass: 'custom-alert',
+          buttons: [
+            {
+              text: 'Cancel',
+              role: 'cancel',
+              cssClass: 'alert-button-cancel'
+            },
+            {
+              text: 'Okay',
+              handler: () => {
+                this.favoriteService.removeFavoriteMovie(this.accountId!, this.sessionId!, this.movie!.id).subscribe(() => {
+                  this.isFavorite = false;
+                });
+              },
+              cssClass: 'alert-button-ok'
+            }
+          ]
         });
+  
+        await alert.present();
       } else {
         this.favoriteService.addFavoriteMovie(this.accountId, this.sessionId, this.movie.id).subscribe(() => {
           this.isFavorite = true;
@@ -66,4 +92,6 @@ export class MovieProfilePage implements OnInit {
       }
     }
   }
+  
+  
 }
